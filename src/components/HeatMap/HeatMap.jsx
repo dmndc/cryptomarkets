b722450aa5b6eee13b7da168683e5b5ab0156fca
Dino Mandic <dinomandic@hotmail.com>
2018-02-19 01:51:21 -0600

    feat: conditionally apply bg color to cards

--- src/components/HeatMap/HeatMap.jsx
+++ src/components/HeatMap/HeatMap.jsx
@@ -9,8 +9,9 @@ const HeatCard = styled.div`
   display: inline-block;
   width: 5em;
   padding: 0.25em;
-  margin: 0.2em;
+  margin: 0.3em;
   border: 1px solid #000;
+  background: ${props => (props.positive ? '#68FD6A' : '#FD7275')};
 `;
 
 class HeatMap extends Component {
@@ -25,14 +26,22 @@ class HeatMap extends Component {
         <Row>
           <Col>
             <h1>Heatmap</h1>
-            {this.props.data.map((el, index) => (
-              <HeatCard>
-                <div className="heatmap--card" key={index}>
-                  <h5>{el.symbol}</h5>
-                  <p>{el.percent_change_24h}</p>
-                </div>
-              </HeatCard>
-            ))}
+
+            {this.props.data.map((el, index) => {
+              let positive = false;
+              if (el.percent_change_24h > 0) {
+                positive = true;
+              }
+
+              return (
+                <HeatCard positive={positive}>
+                  <div className="heatmap--card" key={index}>
+                    <h5>{el.symbol}</h5>
+                    {el.percent_change_24h}
+                  </div>
+                </HeatCard>
+              );
+            })}
           </Col>
         </Row>
       </Container>
