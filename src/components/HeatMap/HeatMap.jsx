371bc9bf48ba5f0e70d21fe6d01a08d575952d5f
Dino Mandic <dinomandic@hotmail.com>
2018-02-19 01:14:15 -0600

    feat: connect to redux

--- src/components/HeatMap/HeatMap.jsx
+++ src/components/HeatMap/HeatMap.jsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
 
-const HeatMap = props => (
-  <div>
-    <Container>
-      <Row>
-        <Col>
-          <h1>Heatmap</h1>
-        </Col>
-      </Row>
-    </Container>
-  </div>
-);
+import { getData } from './../../ducks/data';
 
-export default HeatMap;
+const HeatCard = styled.div`
+  display: inline-block;
+  width: 5em;
+  padding: 0.25em;
+  margin: 0.2em;
+  border: 1px solid #000;
+`;
+
+class HeatMap extends Component {
+  componentDidMount() {
+    getData();
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h1>Heatmap</h1>
+            {this.props.data.map((el, index) => (
+              <HeatCard>
+                <div className="heatmap--card" key={index}>
+                  <h5>{el.symbol}</h5>
+                  <p>{el.percent_change_24h}</p>
+                </div>
+              </HeatCard>
+            ))}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  const { data } = state.data;
+  return {
+    data
+  };
+}
+
+export default connect(mapStateToProps, { getData })(HeatMap);
