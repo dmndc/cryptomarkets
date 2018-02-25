ba865e7ac492de8dc559bc38d87de0b2311d11b5
Dino Mandic <dinomandic@hotmail.com>
2018-02-24 23:26:50 -0600

    feat: add flow

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,48 +1,34 @@
+// @flow
+
 import React from 'react';
 import { connect } from 'react-redux';
 import { Container, Row, Col, Input } from 'reactstrap';
-import {
-  setSearchTerm,
-  setMarketCapFilter,
-  setPriceFilter
-} from './../../ducks/filters';
+import { setSearchTerm, setMarketCapFilter, setPriceFilter } from './../../ducks/filters';
 
-function SearchInput(props) {
-  const {
-    searchTerm,
-    marketCapFilter,
-    priceFilter,
-    setSearchTerm,
-    setMarketCapFilter,
-    setPriceFilter
-  } = props;
+type propTypes = {
+  searchTerm: number,
+  marketCapFilter: number,
+  priceFilter: number,
+  setSearchTerm: Function,
+  setMarketCapFilter: Function,
+  setPriceFilter: Function
+};
+
+function SearchInput(props: propTypes) {
+  const { searchTerm, marketCapFilter, priceFilter, setSearchTerm, setMarketCapFilter, setPriceFilter } = props;
 
   return (
     <Container>
       <Row>
         <Col>
-          <Input
-            onChange={e => setSearchTerm(e.target.value)}
-            value={searchTerm}
-            type="text"
-            placeholder="Search"
-          />
+          <Input onChange={e => setSearchTerm(e.target.value)} value={searchTerm} type="text" placeholder="Search" />
         </Col>
         <Col>
-          <Input
-            type="select"
-            onChange={e => setMarketCapFilter(e.target.value)}
-          >
+          <Input type="select" onChange={e => setMarketCapFilter(e.target.value)}>
             <option value="$1 Billion +">$1 Billion +</option>
-            <option value="$100 Million - $1 Billion">
-              $100 Million - $1 Billion
-            </option>
-            <option value="$10 Million - $100 Million">
-              $10 Million - $100 Million
-            </option>
-            <option value="$1 Million - $100 Million">
-              $1 Million - $100 Million
-            </option>
+            <option value="$100 Million - $1 Billion">$100 Million - $1 Billion</option>
+            <option value="$10 Million - $100 Million">$10 Million - $100 Million</option>
+            <option value="$1 Million - $100 Million">$1 Million - $100 Million</option>
           </Input>
         </Col>
         <Col>
