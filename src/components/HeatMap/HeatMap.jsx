68a7b63b52a4a8a3599132eafd9a7266912c4aee
Dino Mandic <dinomandic@hotmail.com>
2018-02-27 23:32:10 -0600

    refactor: heatMap component

--- src/components/HeatMap/HeatMap.jsx
+++ src/components/HeatMap/HeatMap.jsx
@@ -6,27 +6,27 @@ import styled from 'styled-components';
 import { getData } from './../../ducks/data';
 
 const HeatCard = styled.div`
-  display: inline-block;
+  float: left;
   width: 5em;
   padding: 0.25em;
   margin: 0.3em;
-  border: 1px solid #000;
+  border: 1px solid grey;
   background: ${props => (props.positive ? '#68FD6A' : '#FD7275')};
+  cursor: pointer;
 `;
 
 class HeatMap extends Component {
   componentDidMount() {
-    getData();
+    this.props.getData();
   }
 
   render() {
     console.log(this.props);
     return (
-      <Container>
+      <Container fluid>
         <Row>
           <Col>
             <h1>Heatmap</h1>
-
             {this.props.data.map((el, index) => {
               let positive = false;
               if (el.percent_change_24h > 0) {
@@ -37,7 +37,7 @@ class HeatMap extends Component {
                 <HeatCard positive={positive}>
                   <div className="heatmap--card" key={index}>
                     <h5>{el.symbol}</h5>
-                    {el.percent_change_24h}
+                    {el.percent_change_24h}%
                   </div>
                 </HeatCard>
               );
