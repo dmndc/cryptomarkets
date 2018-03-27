27767ae868305a246ec0ea8542658e7ffd590d86
Dino Mandic <dinomandic@hotmail.com>
2018-03-26 20:16:24 -0500

    refactor: destructure props in AssetCard

--- src/components/AssetCard/AssetCard.jsx
+++ src/components/AssetCard/AssetCard.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 
-const AssetCard = props => {
-  const { name, symbol, price_usd } = props;
-  return (
-    <Card>
-      <CardBody>
-        <CardTitle>
-          <h1>{symbol}</h1>
-          <h2>{name}</h2>
-          <h2>{Number(price_usd).toFixed(2)}</h2>
-        </CardTitle>
-      </CardBody>
-    </Card>
-  );
-};
+const AssetCard = ({ name, symbol, price_usd }) => (
+  <Card>
+    <CardBody>
+      <CardTitle>
+        <h1>{symbol}</h1>
+        <h3>{name}</h3>
+        <h2>$ {Number(price_usd).toFixed(2)}</h2>
+      </CardTitle>
+    </CardBody>
+  </Card>
+);
 
 export default AssetCard;
